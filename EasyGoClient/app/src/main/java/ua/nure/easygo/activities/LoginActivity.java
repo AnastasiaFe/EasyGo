882a31c5dc5a3d053743198b624a4ec52bfe8ed2
fedorenko.anastasiya <nastia.federenko@gmail.com>
2016-11-19 15:17:02 +0200

    registration client

--- EasyGoClient/app/src/main/java/ua/nure/easygo/activities/LoginActivity.java
+++ EasyGoClient/app/src/main/java/ua/nure/easygo/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package ua.nure.easygo.activities;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -68,6 +69,13 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ((TextView)findViewById(R.id.registerLabel)).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
