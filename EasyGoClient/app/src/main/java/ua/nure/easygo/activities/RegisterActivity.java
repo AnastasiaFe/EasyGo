6b967acf4dd933f26e4cca7934b90e4f5bd285ba
fedorenko.anastasiya <nastia.federenko@gmail.com>
2016-11-20 22:23:46 +0200

    reg

---
+++ EasyGoClient/app/src/main/java/ua/nure/easygo/activities/RegisterActivity.java
@@ -0,0 +1,58 @@
+package ua.nure.easygo.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.databinding.generated.callback.OnClickListener;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import easygo.nure.ua.easygoclient.R;
+
+/**
+ * Created by Anastasia on 19.11.2016.
+ */
+
+public class RegisterActivity extends Activity {
+
+
+    private static final int REQUEST = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.registration);
+        TextView addPhotoTextView=(TextView)findViewById(R.id.add_photo);
+        addPhotoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_PICK);
+                i.setType("image/*");
+                startActivityForResult(i, REQUEST);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bitmap img = null;
+
+        if (requestCode == REQUEST && resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //here should be code to write image to db or something like that
+            //image.setImageBitmap(img);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
