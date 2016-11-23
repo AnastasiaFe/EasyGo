e868d54c8d71075fde49c4d2957117dfb209a1e2
Alexander Shylin <ajiekcahdp97@yandex.ru>
2016-11-23 21:07:36 +0200

    New model, changed DAOs to fit new model

--- EasyGoServer/src/ua/nure/easygo/dao/UserDAO.java
+++ EasyGoServer/src/ua/nure/easygo/dao/UserDAO.java
@@ -1,125 +1,16 @@
 package ua.nure.easygo.dao;
 
-import ua.nure.easygo.DBconnect.MySqlConnector;
 import ua.nure.easygo.model.User;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Анна on 23.11.2016.
  */
-public class UserDAO implements IUserDAO {
-    public final String DB_NAME = "stub";
-    public final String TABLE_NAME = "users";
-    private IUserDAO userDAO;
+public interface UserDAO {
+	User createUser(User user);
 
-    public IUserDAO getUserDAO() {
-        return userDAO;
-    }
+	User getUser(String login);
 
-    public void setUserDAO(IUserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
+	User updateUser(User user);
 
-    /*
-    Fields:
-    	public final int id;
-	public final String login;
-	public final String password;
-	public final String email;
-	public final String status;
-     */
-
-    @Override
-    public User createUser(User user) {
-        if (getUser(user.id)==null){
-
-
-            final String queryInsert = "INSERT INTO " + DB_NAME+"."+TABLE_NAME+
-                    " (id, login, password, email, status) values '"+
-                    user.id +"','" + user.login +"','"+user.password+
-                    "','" + user.email+"','" + user.status+"';";
-            try {
-                MySqlConnector.execute(queryInsert);
-                return user;
-            }
-            catch(SQLException e) {
-                e.printStackTrace();
-
-            }}
-        //user already exists
-        return null;
-    }
-
-    @Override
-    public User getUser(int id) {
-        final String query = "SELECT * from" + DB_NAME+"."+TABLE_NAME+" where id='"+id+"';";
-        ArrayList<User> list = new ArrayList<>();
-        try {
-            list = MySqlConnector.selectUser(query);}
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        if(list!=null) {
-            //such user exists
-            return list.get(0);
-        }
-        return null;
-    }
-
-    @Override
-    public User updateUser(User user) {
-        if (getUser(user.id)!=null){
-
-
-            final String queryUpdate = String.format("UPDATE %s.%s" +
-                            " SET login=%s, password=%s, email=%s, status=%s where id=%d",
-                    DB_NAME,TABLE_NAME,
-                    user.login, user.password,
-                    user.email, user.status, user.id);
-
-            try {
-                MySqlConnector.execute(queryUpdate);
-                return user;
-            }
-            catch(SQLException e) {
-                e.printStackTrace();
-
-            }}
-
-        return null;
-    }
-
-    @Override
-    public boolean removeUser(int id) {
-        if (getUser(id)!=null){
-
-            final String queryDelete = String.format("REMOVE FROM %s.%s" +
-                            " where id=%d",
-                    DB_NAME,TABLE_NAME, id);
-            try {
-                MySqlConnector.execute(queryDelete);
-                return true;
-            }
-            catch(SQLException e) {
-                e.printStackTrace();
-
-            }}
-        return false;
-    }
-
-    @Override
-    public List<User> getUserList(String parameters) {
-        List<User> list = new ArrayList<>();
-        final String querySelect = String.format("SELECT * FROM %s.%s",
-                DB_NAME,TABLE_NAME);
-        try{
-            list=MySqlConnector.selectUser(querySelect);}
-        catch (SQLException e){
-            e.printStackTrace();
-        }
-        return list;
-    }
+	boolean removeUser(String login);
 }
--- EasyGoServer/src/ua/nure/easygo/server/ServerUtils.java
+++ EasyGoServer/src/ua/nure/easygo/server/ServerUtils.java
@@ -9,7 +9,7 @@ import com.google.gson.Gson;
 import ua.nure.easygo.DBconnect.MySqlConnector;
 import ua.nure.easygo.model.Entity;
 import ua.nure.easygo.model.GoMap;
-import ua.nure.easygo.model.Mark;
+import ua.nure.easygo.model.Point;
 
 public class ServerUtils {
 
@@ -19,7 +19,7 @@ public class ServerUtils {
 	 *            String for parsing which contains tags (e.g. "#tag1 #tag2")
 	 * @return array of Strings with tags
 	 */
-	private static String[] parseTags(String tagsForParse) {
+	static String[] parseTags(String tagsForParse) {
 		String[] tags = tagsForParse.split("#");
 		for (int i = 0; i < tags.length; i++) {
 			for (int j = 0; j < tags[i].length(); j++) {
@@ -44,39 +44,37 @@ public class ServerUtils {
 		String[] tags = parseTags(tagsForSearch);
 		for (String tag : tags) {
 			maps.addAll(MySqlConnector
-					.selectGoMap("select * from map where map.tags like %" + tag + "% and map.access = `public`"));
+					.selectGoMap("select * from `gomaps` where tags like %" + tag + "% and is_private = 0"));
 		}
 		return maps;
 	}
 
 	/**
-	 * Getting all public maps except maps except maps created by user who
-	 * searches
+	 * Getting all public maps except maps created by user who searches
 	 * 
 	 * @param uID
 	 *            id of map creator
 	 * @return {@link List} of {@link GoMap}
 	 * @throws SQLException
 	 */
-	public static synchronized List<GoMap> getAllPublicMaps(int uID) throws SQLException {
+	public static synchronized List<GoMap> getAllPublicMaps(String login) throws SQLException {
 		List<GoMap> maps = new ArrayList<>();
-		maps.addAll(
-				MySqlConnector.selectGoMap("select * from map where map.access = `public` and creator_id <> " + uID));
+		maps.addAll(MySqlConnector
+				.selectGoMap("select * from gomaps where is_private = 0 and owner_login <> '" + login + "';"));
 		return maps;
 	}
-	
+
 	/**
-	 * Getting all user`s maps 
+	 * Getting all user`s maps
 	 * 
 	 * @param uID
 	 *            id of map creator
 	 * @return {@link List} of {@link GoMap}
 	 * @throws SQLException
 	 */
-	public static synchronized List<GoMap> getAllUserMaps(int uID) throws SQLException {
-		List<GoMap> maps = new ArrayList<>();
-		maps.addAll(
-				MySqlConnector.selectGoMap("select * from map where creator_id = " + uID));
+	public static synchronized List<GoMap> getAllUserMaps(String login) throws SQLException {
+		List<GoMap> maps = null;
+		maps = MySqlConnector.selectGoMap("select * from `gomaps` where owner_login = " + login);
 		return maps;
 	}
 
@@ -91,14 +89,15 @@ public class ServerUtils {
 	}
 
 	/**
-	 * Changes {@link Mark} in th db
+	 * Changes {@link Point} in th db
 	 * 
 	 * @param mk
 	 *            mark to change
 	 * @throws SQLException
 	 */
-	public static synchronized void editMark(Mark mk) throws SQLException {
-		MySqlConnector.execute("update mark set x = " + mk.x + ", y = " + mk.y + ", name = " + mk.name
-				+ ", description = " + mk.description + " where id = " + Integer.toString(mk.id));
+	@Deprecated
+	public static synchronized void editPoint(Point mk) throws SQLException {
+		MySqlConnector.execute("update point set x = " + mk.x + ", y = " + mk.y + ", name = " + mk.name
+				+ ", attribute_values = " + mk.attributeValues + " where id = " + Long.toString(mk.pointId));
 	}
 }
